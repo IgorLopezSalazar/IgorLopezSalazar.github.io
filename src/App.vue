0010a59f964039a2a6c3ac1828f9bb3a88411ca5
Igor Lopez <igor.lopezs@alumni.mondragon.edu>
2022-05-11 15:40:19 +0200

    Conectar Antiguo Nuevo

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+        <div class="opacity"></div>
+        <div class="gravity">
+            <div class="container">
+                <div class=" row">
+                    <div class="col-md-12">
+                        <h1>Igor Lopez Salazar</h1>
+                        <P>My website is being developed</P>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div id="previous">
+            <a href="https://sites.google.com/view/igorlopez" class="links"><span>Visit my previous website </span></a>
+            <a href="./src/segunda/" class="links image" id="newWeb" texto="⚠ Attention! ⚠ This website is under development and is therefore a Alpha version. ">
+                <span>Try my NEW website</span>
+            </a>
+        </div>
+
+        <div class="social-icon">
+            <div class="container">
+                <div class=" row">
+                    <div class="col-md-12">
+                        <div class="icon">
+                            <a href="https://www.linkedin.com/in/igor-lopez-salazar/"><i class="fa fa-linkedin"></i></a>
+                            <a href="https://github.com/IgorLopezSalazar"><i class="fa fa-github"></i></a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  min-height: 100vh;
+  margin: 0 auto;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  .links:hover {
+    background-color: hsla(182, 80%, 36%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  
+}
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const { resolve } = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'src/segunda/index.html'),
+      }
     }
   }
 })
